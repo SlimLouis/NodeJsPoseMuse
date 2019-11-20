e119444162abcaf9b65545b4e0243cb4e01d4501
SlimLouis <56848570+SlimLouis@users.noreply.github.com>
2019-11-20 15:34:27 +0100

    NodeJsPoseMuse

---
+++ routes/Notification.js
@@ -0,0 +1,461 @@
+// index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const notification = express.Router();
+const db = require('../database')
+
+const session = require('express-session')
+var CryptoJS = require('crypto-js');
+var sha1 = require('sha1');
+ 
+var fs = require('fs');
+
+var MySQLStore = require('express-mysql-session')(session);
+const upload_express = require('express-fileupload')
+
+///bcrypt
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+
+// import passport and passport-jwt modules
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const jwt = require('jsonwebtoken');
+
+// ExtractJwt to help extract the token
+let ExtractJwt = passportJWT.ExtractJwt;
+// JwtStrategy which is the strategy for the authentication
+let JwtStrategy = passportJWT.Strategy;
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = 'posemuse';
+
+// parse application/json
+notification.use(bodyParser.json());
+//parse application/x-www-form-urlencoded
+notification.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+notification.use(bodyParser.json());
+
+notification.use(passport.initialize());
+notification.use(passport.session());
+
+//fileupload
+notification.use(upload_express({useTempFiles: true}));
+
+
+
+//parse application/x-www-form-urlencoded
+notification.use(bodyParser.urlencoded({ extended: true }));
+  //express session
+ 
+
+
+
+
+
+  
+  
+  
+  
+
+
+
+
+
+
+  //helper function pre-defined function that can execute queries from and to mysql db
+
+
+  //delete by id
+  const deleteById = async({id})=>
+  {
+    return db.notification.destroy({where: {id:id}});
+  }
+  //filter by gender
+  const notificationByGender = async({ gender_1 }) =>
+  {
+    
+      return db.notification.findAll({where :{gender:gender_1}});
+  };
+
+  const notifications = async({ id_user }) =>
+  {
+    
+      return db.notification.findAll({where :{user_notified:id_user , status:'unopened'}});
+  };
+    //filter by type_use
+    const notificationByType = async({ type_use }) =>
+    {
+      
+        return db.notification.findAll({where :{type_use:type_use}});
+    };
+
+    //filter by style
+    const notificationByStyle = async({ style }) =>
+    {
+    
+        return db.notification.findAll({where :{style:style}});
+    };
+
+
+  
+
+  const createnotification = async ({description,id_user}) =>
+  {
+
+      return await db.notification.create({description,user_notified:id_user,status:'unopened'});
+  };
+
+
+  const getAllnotifications = async(id_user) =>
+  {
+      return db.notification.findAll(
+        {
+          returning:true,where:{user_notified:id_user,status:'unopened'}
+        }
+      );
+  };
+
+  const getnotification = async obj => {
+    return await db.notification.findOne({
+    where: obj,
+  });
+  };
+
+
+ // lets create our strategy for web token
+let strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+    let notification = getnotification({ id: jwt_payload.id });
+    if (notification) {
+      next(null, notification);
+    } else {
+      next(null, false);
+    }
+  });
+  // use the strategy
+  
+  
+  passport.use(strategy);
+
+
+  var cloudinary = require('cloudinary').v2;
+  cloudinary.config({ 
+    cloud_name: 'dprrnjk66', 
+    api_key: '666682518578465', 
+     
+    api_secret: 'PhuV_cPsVZCUzE2xTnvMe3wdlcQ' 
+  });
+
+ 
+  notification.post('/delete_cloud',async function(req,res)
+  {
+
+  })
+  
+
+  //notification delete
+
+  notification.delete('/delete',async function(req,res)
+  {
+    const {id} = req.body ;
+    console.log(id);
+    let notification =  await getnotification({id} );
+var aud = notification.dataValues
+console.log(aud.demo_file)
+let path = 'C:/xampp/htdocs/passeportAuth/uploads/'+aud.demo_file
+cloudinary.uploader.destroy('Demos/'+aud.demo_file.substr(0,aud.demo_file.lastIndexOf('.'))
+,{resource_type:"video"}, function(error,result) {
+  console.log(result, error) });
+
+res.json(notification).then(deleteById({id}));
+
+  })
+  // login route
+  notification.post('/login', async function(req, res, next) { 
+    const { email, password } = req.body;
+    if (email && password) {
+      // we get the notification with the name and save the resolved promise
+      
+      let notification = await getnotification({ email });
+      if (!notification) {
+        res.status(401).json({ msg: 'No such notification found', notification });
+      }
+     if (bcrypt.compareSync(password, notification.password)) {
+        // from now on we’ll identify the notification by the id and the id is
+  // the only personalized value that goes into our token
+        let payload = { name: notification.name };
+        let token = jwt.sign(payload, jwtOptions.secretOrKey);
+        let notification_id = notification.id;
+        req.login(notification_id,function(err)
+        {
+          res.json({ msg: 'ok', token: token });
+          console.log(req.notification);
+          console.log(req.isAuthenticated())
+        })
+      } else {
+        res.status(401).json({ msg: 'Password is incorrect' });
+      }
+    }
+  });
+  //serialization and deserialization
+
+
+
+  
+
+  // get all notifications
+  notification.post('/notifications',  function(req, res) {
+
+    const id_user = req.body.id_user
+ var not =  getAllnotifications(id_user)
+  .then(notification => res.json(notification))
+  .catch(err=>res.json(err)); 
+});
+
+
+const updateNotifications = async({id_user})=>
+{
+  // const id = req.params.id;
+  //         const name = req.body.name;
+  //         const lastname = req.body.lastname;
+  //         const tele = req.body.tele;
+  //         const price = req.body.price;
+  
+  return await db.notification.update(
+    {
+      status : 'opened' 
+    },
+    {
+      returning:true, plain:true,where :{user_notified:id_user}
+    }
+  )
+
+
+}
+
+notification.put('/update',async function(req,res)
+{
+  const id_user = req.body.id_user ;
+  updateNotifications({id_user})
+  .then(result=>res.json(result))
+  .catch(err=>res.json(err).sendStatus(401))
+
+
+
+})
+
+
+// //upload file
+// notification.post('/upload', function(req, res) {
+//   const {id} = req.body ;
+//   console.log(id)
+//   if (!req.files || Object.keys(req.files).length === 0) {
+//     return res.status(400).send('No files were uploaded.');
+//   }
+//   const jsoned = JSON.parse(id);
+//   var real_id = jsoned.id;
+
+//   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+//   let sampleFile = req.files.file;
+//   let file_name = (Date.now()+real_id+sampleFile.name.split(' ').join('')).split(' ').join('');
+
+//   // Use the mv() method to place the file somewhere on your server
+//   sampleFile.mv('uploads/'+file_name, function(err) {
+//     if (err)
+//       return res.status(500).send(err);
+// +
+// createnotification({demo_file:file_name,id_user:real_id})
+// +
+//     res.send('File uploaded!with path'+jsoned.id);
+
+
+
+//   });
+// });
+notification.post('/upload', function(req, res) {
+  var id = req.body ;
+  console.log(id)
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  var real_id = id.id;
+  console.log(real_id);
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  const sampleFile = req.files.file;
+  let ok = req.files.file.path
+  console.log(sampleFile)
+  
+  let file_name = (Date.now()+real_id+sampleFile.name.split(' ').join('')).split(' ').join('');
+  // {public_id: file_name.substr(0,file_name.lastIndexOf('.')),folder:'/Demos'}
+  // Use the mv() method to place the file somewhere on your server
+  cloudinary.uploader.upload(sampleFile.tempFilePath, {public_id: file_name.substr(0,file_name.lastIndexOf('.')),folder:'/Demos',resource_type:"video"}, function(error, result)
+   { console.log(result,"error here"+error) })
++
+createnotification({demo_file:file_name,id_user:real_id}).then(notification => res.json(notification))
+
+    
+
+  });
+
+//upload file TEST
+// notification.post('/upload', function(req, res) {
+//   var id = req.body ;
+//   console.log(id)
+//   if (!req.files || Object.keys(req.files).length === 0) {
+//     return res.status(400).send('No files were uploaded.');
+//   }
+//   var real_id = JSON.parse(id.id);
+// console.log(real_id)
+
+//   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+//   const sampleFile = req.files.file;
+//   // console.log(sampleFile)
+  
+//   let file_name = (Date.now()+real_id.id_user+sampleFile.name.split(' ').join('')).split(' ').join('');
+
+//   // Use the mv() method to place the file somewhere on your server
+//   // sampleFile.mv('uploads/'+file_name, function(err) {
+//   //   if (err)
+//   //     return res.status(500).send(err);
+//   cloudinary.uploader.upload(sampleFile.tempFilePath,{public_id: file_name.substr(0,file_name.lastIndexOf('.')),folder:'/Demos'}, function(error, result)
+//    { console.log(result,error) });
+// +
+// createnotification({demo_file:file_name,id_user:real_id.id_user}).then(notification => res.json(notification))
+
+    
+
+//   });
+
+//single upload
+
+//upload file
+notification.post('/single_upload', function(req, res) {
+
+  var id = req.body
+  console.log(id.id)
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.file;
+  if (!sampleFile)
+  {
+    res.status(251).send("problem with file header")
+  }
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('uploads/'+sampleFile.name, function(err) {
+    if (err)
+      return res.status(360).send("test");
++
+    res.send('File uploaded!with path brobro');
+
+    
+
+  });
+});
+//create notification
+  
+notification.post('/create_notification',async function(req,res,next){
+
+const {description,id_user} = req.body
+
+var notification = await createnotification({description,id_user})
+.then(notifications=>console.log(notifications)+
+res.json(notifications))
+.catch(err=>res.sendStatus(401).json(err))
+
+
+  
+
+});
+
+
+
+ 
+ 
+
+ 
+
+
+
+
+
+//find notification
+
+notification.post('/find_notification', async function(req,res)
+{
+    const {id_user} = req.body ;
+    console.log(id_user);
+     let notification = await getAllnotifications({id_user} );
+
+    res.json(notification);
+})
+
+
+//find notification by gender
+
+notification.post('/notification_gender', async function(req,res)
+{
+    const {gender_1} = req.body ;
+    console.log(gender_1);
+    let notification = await notificationByGender({gender_1} );
+
+    res.json(notification);
+})
+//find notification by type
+
+notification.post('/notification_type', async function(req,res)
+{
+    const {type} = req.body ;
+    console.log(type);
+    // let notification = await notificationByType({type_use:type} );
+
+    // res.json(notification);
+})
+//find notification by style
+
+notification.post('/notification_style', async function(req,res)
+{
+    const {style} = req.body ;
+    console.log(style);
+    // let notification = await notificationByStyle({style} );
+
+    // res.json(notification);
+})
+
+
+  //protection
+  // protected route
+  notification.get('/protected', passport.authenticate('jwt', { session: true }), function(req, res) {
+    const {token} = req.body
+    console.log(token)
+    res.json({ msg: 'Congrats! You are seeing this because you are authorized'});
+    });
+
+
+ 
+
+
+   // app.post('/create_post',passport.authenticate('jwt',{session: false}))
+        // add a basic route
+        notification.get('/', function(req, res) {
+ 
+  console.log(req.notification);
+  console.log(req.isAuthenticated());
+
+   res.json('the notification id of this session depending on the cookie is ' +req.notification);
+  
+});
+
+
+module.exports = notification ;
+
---
+++ routes/Transaction.js
@@ -0,0 +1,490 @@
+// index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const transaction = express.Router();
+const db = require('../database')
+const stripe = require('stripe')("sk_test_4ljsaZQLtNjnFttka3Em9n5X00bRv7V2Tu")
+
+const session = require('express-session')
+
+var MySQLStore = require('express-mysql-session')(session);
+
+const upload_express = require('express-fileupload')
+
+///bcrypt
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+
+// import passport and passport-jwt modules
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const jwt = require('jsonwebtoken');
+
+// ExtractJwt to help extract the token
+let ExtractJwt = passportJWT.ExtractJwt;
+// JwtStrategy which is the strategy for the authentication
+let JwtStrategy = passportJWT.Strategy;
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = 'posemuse';
+
+// parse application/json
+transaction.use(bodyParser.json());
+//parse application/x-www-form-urlencoded
+transaction.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+transaction.use(bodyParser.json());
+
+transaction.use(passport.initialize());
+transaction.use(passport.session());
+
+transaction.use(upload_express({useTempFiles: true}));
+//DATABASE opTIONS
+var options = {
+  host: 'remotemysql.com',
+  port: 3306,
+  user: 'w1yLRuhdZi',
+  password: 'dOyDIS05iR',
+  database: 'w1yLRuhdZi'
+};
+
+// host: 'remotemysql.com',
+// port:3306,
+// database: 'w1yLRuhdZi',
+// transactionname: 'w1yLRuhdZi' ,
+// password: 'dOyDIS05iR',
+// dialect: 'mysql',
+// var sessionStore = new MySQLStore(options);
+
+//parse application/x-www-form-urlencoded
+transaction.use(bodyParser.urlencoded({ extended: true }));
+  //express session
+  transaction.use(session({
+    secret: 'keyboarde cat',
+    resave: false,
+    // store : sessionStore,
+    saveUninitialized: true,
+    //cookie: { secure: true }
+  }))
+
+
+
+var path =require('path')
+
+  var cloudinary = require('cloudinary').v2;
+  cloudinary.config({ 
+    cloud_name: 'dprrnjk66', 
+    api_key: '666682518578465', 
+     
+    api_secret: 'PhuV_cPsVZCUzE2xTnvMe3wdlcQ' 
+  });
+  // console.log(__dirname);
+  // cloudinary.uploader.upload(__dirname+"/batman.png", function(error, result)
+  //  { console.log(result,error) });
+  
+  
+  
+  //upload image using cloudinary
+  // cloudinary.uploader.upload("http://localhost/passeportauth/uploads/images/batman.png", 
+  //  function(error, result) {console.log(result, error)});
+
+  
+  
+  
+
+
+
+
+
+
+
+  //helper function pre-defined function that can execute queries from and to mysql db
+  const updatetransaction = async({profile_pic,id_transaction})=>
+  {
+    // const id = req.params.id;
+    //         const name = req.body.name;
+    //         const lastname = req.body.lastname;
+    //         const tele = req.body.tele;
+    //         const price = req.body.price;
+    
+    return await db.transaction.update(
+      {
+        profile_pic : profile_pic ,
+       
+      },
+      {
+        returning:true, plain:true,where :{id:id_transaction}
+      }
+    )
+
+
+  }
+
+  const createtransaction = async ({id_user,price}) =>
+  {
+
+      return await db.transaction.create({id_user:id_user,amount:price});
+  };
+
+  
+  const getAlltransactions = async() =>
+  {
+      return db.transaction.findAll({      
+        include:[
+          {model: db.User, as: 'host'},
+          {model: db.User, as: 'join'},
+          {model: db.Contest,as:'contest'}
+        ]});
+  };
+
+
+
+  
+  const getAlltransactionsByUserId = async({id_host}) =>
+  {
+      return db.transaction.findAll({      
+        include:[
+          {model: db.User, as: 'host'},
+          {model: db.User, as: 'join'},
+          {model: db.Contest,as:'contest'}
+        ],where:{id_host:id_host}});
+  };
+
+
+  const getAlltransactionsByUserIdJoin = async({id_join}) =>
+  {
+      return db.transaction.findAll({      
+        include:[
+          {model: db.User, as: 'host'},
+          {model: db.User, as: 'join'},
+          {model: db.Contest,as:'contest'}
+        ],where:{id_join:id_join}});
+  };
+
+   
+  const getAlltransactionsById= async({id}) =>
+  {
+      return db.transaction.findAll({      
+        include:[
+          {model: db.User, as: 'host'},
+          {model: db.User, as: 'join'},
+          {model: db.Contest,as:'contest'}        ],where:{id:id}});
+  };
+
+  const gettransaction = async obj => {
+    return await db.transaction.findOne({
+    where: obj
+  });
+  };
+
+
+  const gettransactionProfile = async({ idnumber }) =>
+  {
+    
+      return db.transaction.findAll({where :{id:idnumber},include:[{ model:db.profile}]});
+  };
+
+
+  const getAllContests = async({ idnumber }) =>
+  {
+    
+      return db.transaction.findAll({where :{id:idnumber},include:[{ model:db.Contest}]});
+  };
+
+ // lets create our strategy for web token
+let strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+    let transaction = gettransaction({ id: jwt_payload.id });
+    if (transaction) {
+      next(null, transaction);
+    } else {
+      next(null, false);
+    }
+  });
+  // use the strategy
+  
+  
+  passport.use(strategy);
+
+
+  transaction.post('/paynow',async function(req,res)
+{
+
+console.log(req.body)
+  var token = req.body.token;
+  var price = req.body.price ;
+  var id_user = req.body.id_user
+
+  
+    const charge = await stripe.charges.create({
+      amount: price,
+      currency: 'usd',
+      description: 'Example charge',
+      source: token,
+    })
+    .then(
+      createtransaction({id_user:id_user,price:price/100})
+    )
+    .catch(err=>res.json(err));
+
+ 
+
+
+})
+
+  transaction.post('/success',function(req,res)
+  { 
+
+    
+      var stripeToken = req.body.stripeToken ;
+      var type = req.body.stripeTokenType; 
+      var chargeAmount = Math.round(req.body.amount);
+      var email = req.body.stripeEmail;
+      var currency = req.body.currency;
+      stripe.token.create(
+        {
+          card: {
+            number: '4242424242424242',
+            exp_month: 11,
+            exp_year: 2020,
+            cvc: '314',
+          },
+        },function(err,token)
+        {
+        }
+      )
+
+      // console.log(req.body)
+      // var charge = stripe.charges.create({
+      //   amount: 2000,
+      //   currency: 'usd',
+      //   source: 'tok_amex',
+      //   description: 'Charge for jenny.rosen@example.com',
+      // },function(err,charge)
+      // {
+      //     if (err && err.type=="StripeCardError")
+      //     {
+      //         console.log("your card declined")
+      //         return; 
+      //     }
+      //     // res.sendFile(path.join(__dirname+'../success.html'));
+      //     res.json(charge);
+  
+      //     console.log("this is the amount that has been charged from your account")
+          
+           
+      // })
+  })
+ 
+
+//upload
+transaction.post('/upload', function(req, res) {
+  var id = req.body ;
+  console.log(id)
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  
+  console.log(id.id)
+var I = id.id
+console.log("this is the id",I)
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.file;
+  console.log(sampleFile)
+  
+  let file_name = (Date.now()+I+sampleFile.name.split(' ').join('')).split(' ').join('');
+  console.log("image nameis"+file_name)
+
+  // Use the mv() method to place the file somewhere on your server
+  // sampleFile.mv('uploads/images/'+file_name, function(err) {
+  //   if (err)
+  //     return res.status(500).send(err);
+
+      cloudinary.uploader.upload(sampleFile.tempFilePath,{public_id: file_name.substr(0,file_name.lastIndexOf('.')),folder:'/images'}, function(error, result)
+   { console.log(result,error) });
++
+updatetransaction({profile_pic:file_name,id_transaction:I})
++
+    res.send('File uploaded!with path'+I);
+
+    
+
+  });
+
+  
+//upload file TEST
+// transaction.post('/upload', function(req, res) {
+//   var {id} = req.body ;
+//   if (!req.files || Object.keys(req.files).length === 0) {
+//     return res.status(400).send('No files were uploaded.');
+//   }
+
+//   console.log(id);
+//   var j_id = JSON.parse(id);
+//   var I = j_id.id;
+//   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+//   const sampleFile = req.files.file;
+//   console.log(sampleFile)
+  
+//   let file_name = (Date.now()+I+sampleFile.name.split(' ').join('')).split(' ').join('');
+//   console.log("image nameis"+file_name)
+
+//   console.log(sampleFile.data);
+//   // Use the mv() method to place the file somewhere on your server
+// //   sampleFile.mv('uploads/images/'+file_name, function(err) {
+// //     if (err)
+// //       return res.status(500).send(err);
+// // +}
+// cloudinary.uploader.upload(sampleFile.tempFilePath,{public_id: file_name.substr(0,file_name.lastIndexOf('.')),folder:'/images'}, function(error, result)
+//    { console.log(result,error) });
+  
+// +
+// updatetransaction({profile_pic:file_name,id_transaction:I})
+// +
+//     res.send('File uploaded!with path '+ I);
+
+    
+
+//   });
+// });
+//   transaction.post('/upload', function(req, res) {
+//   var {id} = req.body ;
+//   if (!req.files || Object.keys(req.files).length === 0) {
+//     return res.status(400).send('No files were uploaded.');
+//   }
+
+//   console.log(id);
+//   var j_id = JSON.parse(id);
+//   var I = j_id.id;
+//   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+//   let sampleFile = req.files.file;
+//   console.log(sampleFile)
+  
+//   let file_name = (Date.now()+I+sampleFile.name.split(' ').join('')).split(' ').join('');
+//   console.log("image nameis"+file_name)
+
+//   // Use the mv() method to place the file somewhere on your server
+//   sampleFile.mv('uploads/images/'+file_name, function(err) {
+//     if (err)
+//       return res.status(500).send(err);
+// +
+// updatetransaction({profile_pic:file_name,id_transaction:I})
+// +
+//     res.send('File uploaded!with path'+I);
+
+    
+
+//   });
+// });
+  
+
+//transactionProfile
+transaction.post('/get_profile',async function(req,res){
+
+  const { id } = req.body ;
+  console.log(id)
+
+  let profile = await gettransactionProfile({ idnumber:id })
+  res.json(profile)
+})
+
+transaction.post('/get_current',async function(req,res){
+
+
+  const { id } = req.body ;
+  console.log(id)
+  let transaction = await gettransaction({ id });
+res.json(transaction);
+})
+  // login route
+  transaction.post('/login', async function(req, res, next) { 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const { email, password } = req.body;
+    if (email && password) {
+      // we get the transaction with the name and save the resolved promise
+      
+      let transaction = await gettransaction({ email });
+     // console.log(transaction);
+      if (!transaction) {
+        res.status(401).json({ msg: 'No such transaction found', transaction });
+      }
+    else if (bcrypt.compareSync(password, transaction.password)) {
+        // from now on we’ll identify the transaction by the id and the id is
+  // the only personalized value that goes into our token
+        let payload = { name: transaction.name };
+        let token = jwt.sign(payload, jwtOptions.secretOrKey);
+        let transaction_id = transaction.id;
+        req.login(transaction_id,function(err)
+        {
+          res.json(transaction);
+         // console.log(transaction.dataValues);
+          // console.log(req.transaction);
+          // console.log(req.isAuthenticated())
+        })
+      } else {
+        res.status(401).json({ msg: "Password is incorrect" });
+      } 
+    }
+  });
+  //serialization and deserialization
+
+  transaction.post('/transactions_user', function(req, res) {
+    const {id_host} =req.body ;
+
+
+    getAlltransactionsByUserId({id_host:id_host}).then(result=>res.json(result));
+  });
+
+
+  transaction.post('/transactions_user_join', function(req, res) {
+    const {id_join} =req.body ;
+
+
+    getAlltransactionsByUserIdJoin({id_join:id_join}).then(result=>res.json(result));
+  });
+
+
+  transaction.post('/transaction_id', function(req, res) {
+    const {id} =req.body ;
+
+
+    getAlltransactionsById({id:id}).then(result=>res.json(result));
+  });
+
+
+  // get all transactions
+  transaction.get('/transactions', function(req, res) {
+
+  getAlltransactions().then(transaction => res.json(transaction)); 
+});
+
+//create transaction
+transaction.post('/create_transaction',function(req,res){
+  const {id_host,id_join,id_contest,status,price} = req.body ;
+console.log(id_host,id_join,id_contest,status);
+
+        // Store hash in your password DB.
+
+
+        createtransaction({id_host,id_join,id_contest,status,price})
+        .then(transaction => res.json({transaction,msg:'has been created'}))
+        .catch(e=>{
+            res.status(400).json("problem creating")
+        })
+       
+ 
+ 
+   
+  
+
+})
+
+
+
+
+module.exports = transaction ;
+
